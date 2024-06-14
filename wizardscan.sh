53540b262097ec48605d2564e040b3f00b1faa40
M4nu <96147300+M4nuTCP@users.noreply.github.com>
2024-06-14 16:03:36 +0200

    Update wizardscan.sh

--- wizardscan.sh
+++ wizardscan.sh
@@ -23,6 +23,7 @@ function main(){
 
      if ! command -v jq &> /dev/null
     then
+        echo -e "\n${redColour}[!] Instalango jq...${endColour}"
         sudo apt-get install -y jq > /dev/null 2>&1
     fi
 
